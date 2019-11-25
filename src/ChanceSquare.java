712fb00b520aee1fbe11c48594ae5f26137146d8
Gülce Şirvancı <gulcesirvanci@gmail.com>
2019-11-25 23:46:56 +0300

    when a player lost,fixes their square to ownerless mode and fixes chest and chance squares

--- src/ChanceSquare.java
+++ src/ChanceSquare.java
@@ -6,5 +6,19 @@ public class ChanceSquare extends Square {
 
     @Override
     public void play(Player player) {
+        System.out.println(player.getName() + " is picking a chance card");
+        int i = (int)(Math.random()*4);
+
+        if (i == 0){
+            player.getMoney().decreaseAmount(50);
+            System.out.println(player.getName() + "you have to pay 50 TL to the bank!");
+        }else if(i ==1){
+            player.getMoney().increaseAmount(70);
+            System.out.println(player.getName() + " you found 70 money congratulations!");
+        }else{
+            player.getMoney().increaseAmount(player.getPlayers().size()*10);
+            System.out.println(player.getName() + "Pay 10 times the number of players!");
+        }
+
     }
 }
